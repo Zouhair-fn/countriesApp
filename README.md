acfc78de1edd27d36c8b0d53251e53b634f18711
zouhair <zoufan9@gmail.com>
2023-03-17 13:14:49 +0100

    Ajout d'une page de blog et d'un composant article

--- README.md
+++ README.md
@@ -68,3 +68,5 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+Faire tourner le back : 'json-server --w src/assets/db.json --port 3004'
--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import About from "./pages/About";
+import Blog from "./pages/Blog";
 import Home from "./pages/Home";
 
 const App = () => {
   return (
-  <BrowserRouter>
-  <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/about" element={<About />} />
-    {/* fonctionne si jamais l'url ne correspond à rien de déclaré  */}
-    <Route path="*" element={<Home />} />
-  </Routes>
-  </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/blog" element={<Blog />} />
+        {/* fonctionne si jamais l'url ne correspond à rien de déclaré  */}
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
