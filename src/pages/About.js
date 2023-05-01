f5018505d2641504aabbfadeef924f6ba60ae2e5
zouhair <zoufan9@gmail.com>
2023-05-01 11:09:55 +0200

    changement de nom

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Countries from '../components/Countries';
 import Logo from '../components/Logo';
 import Navigation from '../components/Navigation';
 
@@ -10,9 +9,13 @@ const About = () => {
             <Navigation />
             <h1>A propos</h1>
             <br />
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Neque minima ullam deleniti similique ex alias amet, odit distinctio obcaecati esse eligendi culpa aspernatur? Nobis eveniet ullam beatae atque magni placeat natus alias, nemo officia repellat ipsam amet asperiores iste aliquam maxime assumenda, praesentium nostrum cupiditate vel? Debitis quis dolor saepe delectus praesentium voluptate ea. Soluta, cum obcaecati blanditiis natus, quam tempore aspernatur voluptatem suscipit eius ab eaque temporibus perspiciatis dolorum aliquid porro assumenda asperiores recusandae nulla praesentium corporis beatae et sint similique fuga. Incidunt deleniti ducimus illo. Incidunt eum reprehenderit sequi praesentium! Sint doloribus molestias labore voluptates laborum ut illo quaerat optio repellat provident! Exercitationem, pariatur rerum perspiciatis similique repellendus porro, aliquam quo beatae est nisi odio quibusdam quae aliquid eveniet ut nihil, voluptate maiores nulla corrupti nobis tenetur. Adipisci aperiam tenetur accusantium, neque quas commodi doloribus facilis possimus suscipit esse consectetur harum exercitationem eius quaerat, dicta nesciunt eaque pariatur. Maiores, corrupti. Mollitia voluptas explicabo consequuntur! Libero beatae in nemo possimus qui quos, consequuntur incidunt ipsam temporibus, cum culpa! Ad non officia voluptatum sed veniam! Rerum sapiente accusamus perspiciatis, fugiat ratione minus tempora omnis rem, aperiam earum numquam. Illum hic corrupti voluptates fugit laudantium, id expedita magni quidem cum officia!</p>
+            <p>Le site que nous avons créé est connecté à une API de pays, offrant aux utilisateurs une expérience de navigation fascinante dans le monde entier. Le site est conçu pour afficher les pays du monde avec leur drapeau, leur population et leurs autres informations pertinentes.Le site dispose également d'une fonctionnalité de tri qui vous permet de filtrer les pays par continent. Cela vous permet de découvrir les pays d'un continent particulier.
+            En outre, vous pouvez également trier les pays en fonction de leur population.
+            </p>
             <br />
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident, vitae quod nobis temporibus aliquid, minus labore aut iure accusamus repudiandae ipsa quasi unde? Molestiae iusto facilis at officia dolor atque quis laboriosam, nostrum, velit a facere commodi rerum alias, quo eius iure? Explicabo error debitis, cumque, laboriosam excepturi quo, temporibus impedit aperiam qui delectus eligendi. Cupiditate eos officiis ducimus, quo, nulla beatae fugit nam porro earum dignissimos laudantium esse totam velit natus quibusdam libero eaque, tenetur fugiat distinctio. Ab quod, odio, tenetur nisi ut commodi pariatur ex, saepe vero eaque error consequuntur ipsum velit cupiditate dignissimos et quam. At, repellat!</p>
+            <p>Dans l'ensemble, notre site connecté à une API de pays est un excellent outil pour explorer le monde et découvrir chaque pays.
+            Nous espérons que cette expérience de navigation immersive vous inspirera à voyager davantage et à en apprendre davantage sur notre monde incroyable.
+            </p>
         </div>
     );
 };
